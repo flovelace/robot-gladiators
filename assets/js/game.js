11ce9bf75f2836896942bfc93587542f1846ccd4
fairfax <lovelace.af@gmail.com>
2021-09-05 20:37:53 -0400

    added start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -69,11 +69,65 @@ var fight = function(enemyName) {
   }
 };
 
+// function to start a new game
+var startGame = function() {
+  //reset player stats
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
+
+  for (var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+      window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+
+      var pickedEnemyName = enemyNames[i];
+
+      enemyHealth = 50;
+
+      fight(pickedEnemyName);
+    }
+    else {
+      window.alert("You have lost your robot in battle! Game Over!");
+      break;
+
+    }
+  }
+
+  //after the game loop end, player is either out of health or enemies to fight, the endGame function now runs
+  endGame();
+
+  //play again
+  startGame();
+};
+
+// function to end the entire game
+var endGame = function() {
+  window.alert("The game has now ended. Let's see how you did!");
+
+  // if the player is still alive, the player wins
+  if (playerHealth > 0) {
+    window.alert("Great job, you've surived the game! You now have a score of " + playerMoney + ".");
+  }
+  else {
+    window.alert("You've lost your robot in battle.");
+  }
+  // ask the player if they would like to play again
+  var playAgainConfirm = window.confirm("Would you like to play again?");
+
+  if (playAgainConfirm) {
+    //restart the game
+    startGame();
+  }
+  else {
+    window.alert("Thank you for playing Robot Gladiators! Come back soon!")
+  }
+};
+
 // fight each enemy-robot by looping over them and fighting them one at a time
 for (var i = 0; i < enemyNames.length; i++) {
   // if player is still alive, keep fighting
   if (playerHealth > 0) {
-    // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+    // let player know what round they are in
     window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
 
     // pick new enemy to fight based on the index of the enemyNames array
@@ -90,3 +144,6 @@ for (var i = 0; i < enemyNames.length; i++) {
     break;
   }
 }
+
+//start game when page loads
+startGame();
