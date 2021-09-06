9e2a4aa26f01173534e737acbcef16bc7fb5b3b7
fairfax <lovelace.af@gmail.com>
2021-09-06 15:35:54 -0400

    fixed player name bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -160,8 +160,20 @@ var shop = function() {
   }
 };
 
+// function to set name
+var getPlayerName = function() {
+  var name = "";
+
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+
+  console.log("Your robot's name is " + name);
+  return name;
+};
+
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
@@ -172,7 +184,7 @@ var playerInfo = {
   },
   refillHealth: function() {
     if (this.money >= 7) {
-      window.alert("Refilling player's health by 20 for 7 bucks");
+      window.alert("Refilling player's health by 20 for 7 bucks.");
       this.health += 20;
       this.money -= 7;
     }
